7535c2db8c1cd1013efade01c63c4cf1ea950e2d
cc15 <chiragwadhera15@gmail.com>
2017-03-31 21:01:19 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/cc15/test/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.cc15.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new MyNavigationDrawer(this));
+    }
+}
---
+++ app/src/main/java/com/example/cc15/test/MyNavigationDrawer.java
@@ -0,0 +1,33 @@
+package com.example.cc15.test;
+
+import android.app.Activity;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+
+
+public class MyNavigationDrawer implements NavigationView.OnNavigationItemSelectedListener {
+
+    Activity activity;
+
+    public MyNavigationDrawer(Activity activity) {
+        this.activity = activity;
+
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        DrawerLayout drawer = (DrawerLayout) activity.findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+
+}
